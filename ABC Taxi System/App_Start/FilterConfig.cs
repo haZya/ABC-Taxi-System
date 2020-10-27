ea0b3cfc3937c2eb0f7ba50d4784ee8d0a296614
Hasitha Wickramasinghe <hasitha.wickramasinghe@hotmail.com>
2020-10-27 11:35:03 +0530

    Add project files.

---
+++ ABC Taxi System/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ABC_Taxi_System
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ABC Taxi System/Models/FindAccount.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ABC_Taxi_System.Models
+{
+    /// <summary>
+    /// Model for Find Account
+    /// </summary>
+    public class FindAccount
+    {
+        [Required]
+        [Display(Name = "Username or Email")]
+        public string UsernameOrEmail { get; set; }
+    }
+}
